59136ae717da3eef3f25a2062b2aeeaf706dd3e9
chengwen-zheng <chengwenzheng1222@gmail.com>
2024-06-16 15:11:45 +0800

    feat: thiserror and anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use ecosystem::MyError;
+
+fn main() -> Result<(), anyhow::Error> {
+    println!(
+        "size of anyhow::Error: {}",
+        std::mem::size_of::<anyhow::Error>()
+    );
+
+    println!(
+        "size of std::io::Error is: {}",
+        std::mem::size_of::<std::io::Error>()
+    );
+    // size of parse int error
+    println!(
+        "size of std::num::ParseIntError is: {}",
+        std::mem::size_of::<std::num::ParseIntError>()
+    );
+
+    // serde json error
+    println!(
+        "size of serde_json::Error is: {}",
+        std::mem::size_of::<serde_json::Error>()
+    );
+
+    // size of String
+    println!("size of String is: {}", std::mem::size_of::<String>());
+
+    // size of MyError
+    println!("size of MyError is: {}", std::mem::size_of::<MyError>());
+
+    fail_with_error()?;
+    Ok(())
+}
+
+fn fail_with_error() -> Result<(), MyError> {
+    Err(MyError::Custom("This is a custom error".to_string()))
 }
