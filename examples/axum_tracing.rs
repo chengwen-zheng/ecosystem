ea073e7b0b4c64e03623ef8ee499dfe4a4b768a5
chengwen-zheng <chengwenzheng1222@gmail.com>
2024-06-22 14:57:33 +0800

    feat: support opentelemetry for axum_tracing

--- examples/axum_tracing.rs
+++ examples/axum_tracing.rs
@@ -2,7 +2,15 @@ use std::time::Duration;
 
 use anyhow::Ok;
 use axum::{routing::get, Router};
+use opentelemetry::Key;
+use opentelemetry_otlp::WithExportConfig;
+use opentelemetry_sdk::{
+    runtime,
+    trace::{self, RandomIdGenerator, Tracer},
+    Resource,
+};
 use tokio::{
+    join,
     net::TcpListener,
     time::{sleep, Instant},
 };
@@ -16,23 +24,28 @@ use tracing_subscriber::{
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let file_appender = tracing_appender::rolling::daily("/tmp/logs", "ecosystem.log");
-
-    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-
+    // console layer  for tracing-subscriber
     let console = fmt::Layer::new()
         .with_span_events(FmtSpan::CLOSE)
         .pretty()
         .with_filter(LevelFilter::DEBUG);
 
+    // file appender layer for tracing-subscriber
+    let file_appender = tracing_appender::rolling::daily("/tmp/logs", "ecosystem.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
     let file = fmt::Layer::new()
         .with_writer(non_blocking)
         .pretty()
         .with_filter(LevelFilter::INFO);
 
+    // opentelemetry layer for tracing-subscriber
+    let tracer = init_tracer()?;
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
     tracing_subscriber::registry()
         .with(console)
         .with(file)
+        .with(telemetry)
         .init();
 
     let addr = "0.0.0.0:8080";
@@ -50,15 +63,58 @@ async fn index_handler() -> &'static str {
     debug!("index handler started");
     sleep(Duration::from_millis(10)).await;
     let ret = long_task().await;
-    info!(http.status = 200, "index handler completed");
+    info!(http.status_code = 200, "index handler completed");
     ret
+}
+
+#[instrument]
+async fn task1() {
+    sleep(Duration::from_millis(10)).await;
+}
+
+#[instrument]
+async fn task2() {
+    sleep(Duration::from_millis(50)).await;
+}
+
+#[instrument]
+async fn task3() {
+    sleep(Duration::from_millis(30)).await;
 }
 
 #[instrument]
 async fn long_task() -> &'static str {
     let start = Instant::now();
-    sleep(Duration::from_millis(112)).await;
+    let sl = sleep(Duration::from_millis(112));
+
+    // spawn multiple tasks
+
+    let t1 = task1();
+    let t2 = task2();
+    let t3 = task3();
+    join!(sl, t1, t2, t3);
     let elapsed = start.elapsed().as_millis() as u64;
     warn!(app.task_duration = elapsed, "task takes too long");
     "Hello, World!"
+}
+
+fn init_tracer() -> anyhow::Result<Tracer> {
+    let tracer = opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint("http://localhost:4317"),
+        )
+        .with_trace_config(
+            trace::config()
+                .with_id_generator(RandomIdGenerator::default())
+                .with_max_attributes_per_span(32)
+                .with_max_attributes_per_span(32)
+                .with_resource(Resource::new(vec![
+                    Key::new("service.name").string("axum-tracing")
+                ])),
+        )
+        .install_batch(runtime::Tokio)?;
+    Ok(tracer)
 }
